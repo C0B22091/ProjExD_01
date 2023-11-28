a227e2dd7373afa5c37af253e4a88ac3c1e76336
takizawa <c0b22091d4@edu.teu.ac.jp>
2023-11-28 15:15:56 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,10 +2,23 @@ import sys
 import pygame as pg
 import random
 
-WIDTH, HEIGHT = 1600, 900
+WIDTH, HEIGHT = 1200, 600
 
 delta = {pg.K_UP:(0, -5), pg.K_DOWN:(0, +5), 
          pg.K_LEFT:(-5, 0), pg.K_RIGHT:(+5, 0)}
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内にいるかどうか判定。
+    引数 rct: こうかとんor爆弾SurfaceのRect
+    戻り値: 横方向, 縦方向はみ出し判定結果（画面内: True/画面外: False）
+    """
+    yoko, tate = True, True
+    if 0 > rct.left or WIDTH < rct.right:
+        yoko = False
+    if 0 > rct.top or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
 
 
 def main():
@@ -16,7 +29,7 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     clock = pg.time.Clock()
     kk_rct = kk_img.get_rect()  # 練習3
-    kk_rct.center = 900, 400  # 練習3 こうかとんの初期座標
+    kk_rct.center = WIDTH/2, HEIGHT/2  # 練習3 こうかとんの初期座標
     bb_img = pg.Surface((20, 20))  # 練習1 透明のSurface
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 練習1 赤い半径10の円を描画
     bb_img.set_colorkey((0, 0, 0))  # 練習1 黒を透過
@@ -37,16 +50,24 @@ def main():
             if key_lst[k]:  # キーが押されたら
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
-        kk_rct.move_ip(sum_mv)
 
         screen.blit(bg_img, [0, 0])
+        kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):  # 練習4 こうかとんが画面外なら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        img_rct.move_ip((vx, vy))  # 練習2
+        img_rct.move_ip(vx, vy)  # 練習2
+        yoko, tate = check_bound(img_rct)  # 練習4 爆弾のはみだし判定
+        if not yoko:  # 横方向にはみ出たら
+            vx *= -1
+        if not tate:
+            vy *= -1
+        img_rct.move_ip(vx, vy)
         screen.blit(bb_img, img_rct)
     
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
@@ -54,3 +75,5 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
+
+
